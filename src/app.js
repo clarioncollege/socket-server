d3222ab9939f982aa741868d94c38919db56f5ef
certifiedTboy <etosin70@gmail.com>
2025-12-06 16:07:03 +0100

    add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const { getAllRooms } = require("./models/room-model");
 const app = express();
 
+app.use(cors({ origin: ["http://localhost:5173"] }));
 app.use(express.json());
 
 /**
